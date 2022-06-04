68544798f209fbbdb3fcf0d1f4f6682792fba9c7
MarioRojoVicente <mario.rojo.vicente@alumnos.upm.es>
2022-06-04 11:11:26 +0200

    members view

--- src/scripts/members.js
+++ src/scripts/members.js
@@ -1,51 +1,43 @@
 let members_mock = [
     {
         name: "Pedro",
-        lastnames: "Jimenez de la Paz"
+        lastnames: "Jimenez de la Paz",
+        tags: ["Developer", "Designer", "Engeneer"]
     },
     {
         name: "Alberto",
-        lastnames: "Pedrero Degüelles"
+        lastnames: "Pedrero Degüelles",
+        tags: ["Developer", "Designer", "Engeneer"]
     },
     {
         name: "Elena",
-        lastnames: "Nito del Bosque"
+        lastnames: "Nito del Bosque",
+        tags: ["Developer", "Designer", "Engeneer"]
     },
     {
         name: "Elbert",
-        lastnames: "Galarga"
+        lastnames: "Galarga",
+        tags: ["Developer", "Designer", "Engeneer"]
     },
     {
         name: "Pedro",
-        lastnames: "Jimenez de la Paz"
+        lastnames: "Jimenez de la Paz",
+        tags: ["Developer", "Designer", "Engeneer"]
     },
     {
         name: "Alberto",
-        lastnames: "Pedrero Degüelles"
+        lastnames: "Pedrero Degüelles",
+        tags: ["Developer", "Designer", "Engeneer"]
     },
     {
         name: "Elena",
-        lastnames: "Nito del Bosque"
+        lastnames: "Nito del Bosque",
+        tags: ["Developer", "Designer", "Engeneer"]
     },
     {
-        name: "Elbert",
-        lastnames: "Galarga"
-    },
-    {
-        name: "Pedro",
-        lastnames: "Jimenez de la Paz"
-    },
-    {
-        name: "Alberto",
-        lastnames: "Pedrero Degüelles"
-    },
-    {
-        name: "Elena",
-        lastnames: "Nito del Bosque"
-    },
-    {
-        name: "Elbert",
-        lastnames: "Galarga"
+        name: "Elbertttttttttttttttttttttttttttttttttttttttttt",
+        lastnames: "Galarga",
+        tags: ["Software Developerrrrrrrrrrrrrrrrr", "Designer", "Engeneer", "Developer", "Designer", "Engeneer"]
     }
 ]
 
@@ -53,17 +45,32 @@ async function loadMembersList(){
 
     let members = members_mock; // ahora se moquea pero aquí iría la llamada al endpoint de cgetMembers
     
-    console.log(members);
     let main = document.getElementsByTagName("main")[0];
-    let div = document.createElement("div");
+    let container = document.createElement("div");
+    container.setAttribute("id", "membersCardsContainer");
     for (let member of members){
-        let link = document.createElement("a");
-        link.setAttribute("href", "https://example.com");
-        let linkText = document.createTextNode( member.name +" "+ member.lastnames);
-        link.appendChild(linkText);
-        div.appendChild(link);
+        let div = document.createElement("div");
+        div.setAttribute("id", "memberCard");
+        let img = document.createElement("img");
+        img.setAttribute("src", "https://picsum.photos/200/200")
+        let span = document.createElement("span");
+        let divText = document.createTextNode( member.name +" "+ member.lastnames);
+        div.appendChild(img);
+        span.appendChild(divText);
+        div.appendChild(span);
+        container.appendChild(div);
+
+        let tagDiv = document.createElement("div");
+        tagDiv.setAttribute("id", "tagContainer");
+        for (let tag of member.tags){
+            let span = document.createElement("span");
+            let text = document.createTextNode(tag);
+            span.appendChild(text);
+            tagDiv.appendChild(span)
+        }
+        div.appendChild(tagDiv);
     }
-    main.appendChild(div)
+    main.appendChild(container)
 
 }
 
